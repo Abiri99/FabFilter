8b44538926756e7f113566c538a2ea081b2a8bfa
alireza <abiriali78@gmail.com>
2020-10-03 22:25:41 +0330

    Added animations

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,61 +10,107 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
 
+  Animation _xAxisPositionAnimation;
+  Animation _yAxisPositionAnimation;
 
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600),
+      reverseDuration: Duration(milliseconds: 600),
+    );
+    _xAxisPositionAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeOut,
+      reverseCurve: Curves.easeOut,
+    );
+    _yAxisPositionAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+      reverseCurve: Curves.easeIn,
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColorLight,
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          CustomScrollView(
-            slivers: [
-              SliverPersistentHeader(
-                delegate: CustomAppBar(),
-                pinned: true,
-              ),
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, position) {
-                    return ListItem(
-                      horizontalMargin: 16,
-                    );
-                  },
+      body: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) => Stack(
+          fit: StackFit.expand,
+          children: [
+            CustomScrollView(
+              slivers: [
+                SliverPersistentHeader(
+                  delegate: CustomAppBar(),
+                  pinned: true,
                 ),
-              ),
-            ],
-          ),
-          Positioned(
-            bottom: 24,
-            right: 24,
-            child: Container(
-              height: 64,
-              width: 64,
-              padding: const EdgeInsets.all(8),
-              child: Center(
-                child: FilterIcon(),
-              ),
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColorDark,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(100),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0, 15),
-                    blurRadius: 15,
-                    spreadRadius: -8,
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, position) {
+                      return Transform.scale(
+                        scale: 1.0,
+                        // scale: 1.0 - _controller.value * 0.1,
+                        child: Opacity(
+                          opacity: 1 - _controller.value * 0.4,
+                          child: ListItem(
+                            horizontalMargin: _controller.value * 12 + 18,
+                            verticalPadding: 20 - _controller.value * 4,
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                ],
+                ),
+              ],
+            ),
+            Positioned(
+              bottom: (_yAxisPositionAnimation.value *
+                  (MediaQuery.of(context).size.height / 3 - 56) as double),
+              right: (_xAxisPositionAnimation.value *
+                  (MediaQuery.of(context).size.width / 2 - 56) as double),
+              child: GestureDetector(
+                onTap: () {
+                  if (_controller.status == AnimationStatus.completed) {
+                    _controller.reverse();
+                  } else {
+                    _controller.forward();
+                  }
+                },
+                child: Container(
+                  height: 64,
+                  width: 64,
+                  margin: const EdgeInsets.all(24),
+                  padding: const EdgeInsets.all(8),
+                  child: Center(
+                    child: FilterIcon(),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColorDark,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(100),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 15),
+                        blurRadius: 15,
+                        spreadRadius: -8,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/list_item.dart
+++ lib/list_item.dart
@@ -1,95 +1,183 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'line.dart';
 
-class ListItem extends StatelessWidget {
+class ListItem extends StatefulWidget {
   final double horizontalMargin;
+  final double verticalPadding;
 
   ListItem({
     this.horizontalMargin = 16,
+    this.verticalPadding = 16,
   });
 
   @override
+  _ListItemState createState() => _ListItemState();
+}
+
+class _ListItemState extends State<ListItem>
+    with SingleTickerProviderStateMixin {
+
+  AnimationController _controller;
+  Animation _expansionAnimation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+      reverseDuration: Duration(milliseconds: 1000),
+    );
+    _expansionAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+      reverseCurve: Curves.easeInOut,
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(
-        horizontal: horizontalMargin,
-        vertical: 8,
-      ),
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 16,
-      ),
-      decoration: BoxDecoration(
-        color: Theme.of(context).cardColor,
-        borderRadius: BorderRadius.all(
-          Radius.circular(4),
-        ),
-      ),
-      child: Row(
-        children: [
-          Icon(
-            Icons.arrow_forward_ios,
-            color: Theme.of(context).primaryColorLight,
-            size: 16,
+    return GestureDetector(
+      onTap: () {
+        if (_controller.status == AnimationStatus.completed) {
+          _controller.reverse();
+        } else {
+          _controller.forward();
+        }
+      },
+      child: AnimatedBuilder(
+        animation: _expansionAnimation,
+        builder: (context, child) => Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: widget.horizontalMargin - _expansionAnimation.value * 8,
+            vertical: 8,
           ),
-          SizedBox(
-            width: 8,
+          padding: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: widget.verticalPadding,
           ),
-          Expanded(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 4,
-                      child: Line(
-                        color: Theme.of(context).primaryColorLight,
-                        height: 10,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 3,
-                      child: Line(
-                        color: Theme.of(context)
-                            .primaryColorLight
-                            .withOpacity(0.5),
-                        height: 10,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 16,
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Line(
-                        color: Theme.of(context)
-                            .primaryColorLight
-                            .withOpacity(0.5),
-                        height: 10,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: SizedBox(),
-                    )
-                  ],
-                ),
-              ],
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+            borderRadius: BorderRadius.all(
+              Radius.circular(4),
             ),
           ),
-        ],
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Transform.rotate(
+                    angle: _expansionAnimation.value * (pi / 2),
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Theme.of(context).primaryColorLight,
+                      size: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 4,
+                              child: Line(
+                                color: Theme.of(context).primaryColorLight,
+                                height: 10,
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Container(),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: Line(
+                                color: Theme.of(context)
+                                    .primaryColorLight
+                                    .withOpacity(0.5),
+                                height: 10,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 16,
+                            ),
+                            Expanded(
+                              flex: 2,
+                              child: Line(
+                                color: Theme.of(context)
+                                    .primaryColorLight
+                                    .withOpacity(0.5),
+                                height: 10,
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: SizedBox(),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                heightFactor: _controller.value,
+                child: SizedBox(height: 8,),
+              ),
+              ClipRect(
+                child: Align(
+                  alignment: Alignment.center,
+                  heightFactor: _controller.value,
+                  child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    // itemExtent: 24,
+                    itemCount: 4,
+                    itemBuilder: (context, position) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 2,),
+                        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8,),
+                        color: position.isEven ? Theme.of(context).primaryColorLight.withOpacity(0.3) : Colors.transparent,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Line(
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                            SizedBox(width: 8,),
+                            Expanded(
+                              child: Line(
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
